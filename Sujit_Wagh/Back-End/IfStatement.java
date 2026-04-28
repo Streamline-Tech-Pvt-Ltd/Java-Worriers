e9cbe44abd5435838efc7a2608df6cd006d7d569
Sujit-Wagh <sujitwagh532@gmail.com>
2026-04-28 13:41:47 +0530

    loop file

---
+++ Sujit_Wagh/Back-End/IfStatement.java
@@ -0,0 +1,19 @@
+public class IfStatement {
+    public static void main(String[] args)
+    {
+        int a=10;
+
+        if(a>0)
+        {
+            System.out.println("a is positive");
+        }
+        else if(a<0)
+        {
+            System.out.println("a is negative");
+        }
+        else
+        {
+            System.out.println("a is zero");
+        }
+    }
+}
