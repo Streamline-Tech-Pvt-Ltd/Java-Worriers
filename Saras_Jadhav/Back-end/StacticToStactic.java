2297096c819f625326188297e173e43f64429d1e
sarasjadhav45 <sarasjadhav27@gmail.com>
2026-05-11 17:29:28 +0530

    java code

---
+++ Saras_Jadhav/Back-end/StacticToStactic.java
@@ -0,0 +1,11 @@
+public class StaticToStatic {
+
+	public static void main(String[] args) {
+		System.out.println("hello world..");
+		add(2,3);
+
+	}
+	public static void add(int x, int y) {
+		System.out.println(x+y);
+	}
+}
