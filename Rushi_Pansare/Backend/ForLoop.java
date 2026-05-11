f2bd6ae987ee94f9d2a9498fda6e3ce00fe13744
Rishikeshpansare <rushipansare74@gmail.com>
2026-05-11 17:20:00 +0530

    Loop

---
+++ Rushi_Pansare/Backend/ForLoop.java
@@ -0,0 +1,19 @@
+package rushi.main.loops;
+
+public class ForLoop {
+	/*	public static void main(String[] args) {
+		for(int i =1;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			System.out.println(2*i);
+		}
+		*/	
+	
+	
+}
---
+++ Rushi_Pansare/Backend/Pattern.java
@@ -0,0 +1,15 @@
+package rushi.main.loops;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		for(int i =10;i>=1;i--) {
+		for(int j=1;j<=5;j++) {
+			System.out.print("*");
+					}
+		System.out.println();
+		}
+		
+	}
+
+}
