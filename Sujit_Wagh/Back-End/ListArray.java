66033959d3d60f5e7d83289be4bebfa39a62a336
Sujit-Wagh <sujitwagh532@gmail.com>
2026-06-30 17:51:23 +0530

    Collection ArrayList

---
+++ Sujit_Wagh/Back-End/ListArray.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ListArray {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(10);
+		arr.add(20);
+		arr.add(30);
+		System.out.println(arr);
+
+	}
+
+}
---
+++ Sujit_Wagh/Back-End/MultipleValueStored.java
@@ -0,0 +1,18 @@
+public import java.util.ArrayList;
+
+public class MultipleValueStored {
+
+	public static void main(String[] args) {
+		ArrayList<Object> ar = new ArrayList<Object>();
+		ar.add(10);
+		ar.add("Sujit");
+		ar.add(56.6f);
+		ar.add(34564l);
+	
+System.out.println(ar);
+	}
+
+}
+ {
+    
+}
